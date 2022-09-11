73a5139d9609cd1a81be3ce3623ecc9c8f329eaa
Albert <gamakafos@gmail.com>
2022-09-11 23:32:17 +0200

    just a servlet without any analis

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -8,11 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.User;
+import utils.JDBCUtils;
 
 public class UserDAO {
-	private String jdbcUrl= "jdbc:mysql://localhost:3306/java_demo?useSSL=false";
-	private String jdbcUsername = "root";
-	private String jdbcPassword = "user";
 	
 	//define sql statements
 	private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES (?, ?, ?);";
@@ -22,22 +20,13 @@ public class UserDAO {
 	private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
 	
 	
-	// funcion of getting connection
-	protected Connection getConnection() {
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}
+
 	
 	
 	
 	//insert user into db
 	public void insertUser(User user) {
-		try(Connection connection = getConnection();
+		try(Connection connection = JDBCUtils.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)){
 			preparedStatement.setString(1, user.getName());
 			preparedStatement.setString(2, user.getEmail());
@@ -53,7 +42,7 @@ public class UserDAO {
 	//select user from db
 	public User selectUser(int id) {
 		User user = null;
-		try(Connection connection = getConnection();
+		try(Connection connection = JDBCUtils.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID)){
 			
 			preparedStatement.setInt(1, id);
@@ -77,7 +66,7 @@ public class UserDAO {
 	public List<User> selectAllUsers(){
 		List<User> users = new ArrayList<>();
 		
-		try(Connection connection = getConnection();
+		try(Connection connection = JDBCUtils.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS)){
 			
 			ResultSet resultSet = preparedStatement.executeQuery();
@@ -101,7 +90,7 @@ public class UserDAO {
 	//delete user from db
 	public boolean deleteUser(int id) {
 		boolean rowDeleted = false;
-		try(Connection connection = getConnection();
+		try(Connection connection = JDBCUtils.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL)){
 			
 			preparedStatement.setInt(1, id);
@@ -121,7 +110,7 @@ public class UserDAO {
 	//update user in db
 	public boolean updateUser(User user) {
 		boolean rowUpdated = false;
-		try(Connection connection = getConnection();
+		try(Connection connection = JDBCUtils.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL)){
 			
 			//didnt realy get, how it will update our uesr
